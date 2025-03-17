888f55fff0552e671ed3216097910d46ffe57b33
zelcam4ever <ezequiel.padillo.01@gmail.com>
2025-03-17 20:13:17 +0100

    chore: temporal agent authoring

--- Assets/Scripts/Autoring/AgentAutoring.cs
+++ Assets/Scripts/Autoring/AgentAutoring.cs
@@ -10,28 +10,35 @@ namespace EcsTraining
         public bool RequestDecision;
         public int StepCount;
         public int EpisodeId;
-    }
-    
-    class Baker : Baker<AgentAutoring>
-    {
-        public override void Bake(AgentAutoring authoring)
+        public int AgentInfoId;
+        
+        private class Baker : Baker<AgentAutoring>
         {
+            public override void Bake(AgentAutoring authoring)
+            {
             
-            var entity = GetEntity(TransformUsageFlags.None);
-            AddComponent(entity, new AgentSimple() //TODO: Change proper agentsº
-            {
-                Reward = authoring.Reward,
-                RequestAction = authoring.RequestAction,
-                RequestDecision = authoring.RequestDecision,
-                StepCount = authoring.StepCount,
-                EpisodeId = authoring.EpisodeId
-            });
-            AddComponent(entity, new Observation() 
-            {
-                Value = 5
-            });
+                var entity = GetEntity(TransformUsageFlags.None);
+                AddComponent(entity, new Agent() 
+                {
+                    Reward = authoring.Reward,
+                    RequestAction = authoring.RequestAction,
+                    RequestDecision = authoring.RequestDecision,
+                    StepCount = authoring.StepCount,
+                    EpisodeId = authoring.EpisodeId,
+                    AgentInfoId = authoring.AgentInfoId
+                });
+                AddComponent(entity, new Observation() 
+                {
+                    Value = 5
+                });
+                AddComponent(entity, new RemotePolicy());
+                AddComponent(entity, new BrainSimple() 
+                {
+                    FullyQualifiedBehaviorName = "a"
+                });
+            }
         }
-
-      
     }
+
+    
 }
