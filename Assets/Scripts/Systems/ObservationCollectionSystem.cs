071a368d6c7cfe9714509a2011e98cba92e2bf18
zelcam4ever <ezequiel.padillo.01@gmail.com>
2025-07-02 17:11:57 +0200

    feat: observation collection job

---
+++ Assets/Scripts/Systems/ObservationCollectionSystem.cs
@@ -0,0 +1,148 @@
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Burst.Intrinsics;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+using static Unity.Entities.SystemAPI;
+
+namespace EcsTraining
+{
+    [UpdateAfter(typeof(IncrementStepSystem))]
+    public partial struct ObservationCollectionSystem : ISystem
+    {
+        private EntityQuery _AgentQuery;
+        private ComponentLookup<LocalTransform> _TransformLookup;
+        private ComponentLookup<PhysicsVelocity> _VelocityLookup;
+        
+        private BufferTypeHandle<ObservationSource> _SourceBufferTypeHandle;
+        private  BufferTypeHandle<ObservationValue> _ValueBufferTypeHandle;
+        
+        private EntityTypeHandle _entityTypeHandle;
+        
+
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            // Define the query for entities that need observations gathered.
+            _AgentQuery = new EntityQueryBuilder(Allocator.Temp)
+                .WithAll<ObservationSource, ObservationValue>()
+                .Build(ref state);
+            
+            // We require these components to exist for the job, so we get their lookups.
+            // The 'true' argument means we will only read from them.
+            _TransformLookup = state.GetComponentLookup<LocalTransform>(true);
+            _VelocityLookup = state.GetComponentLookup<PhysicsVelocity>(true);
+            _entityTypeHandle = GetEntityTypeHandle();
+
+            _SourceBufferTypeHandle = state.GetBufferTypeHandle<ObservationSource>(true); // ReadOnly
+            _ValueBufferTypeHandle = state.GetBufferTypeHandle<ObservationValue>(false); // Read/Write
+        
+            // Don't run the system if no agents exist.
+            state.RequireForUpdate(_AgentQuery);
+        }
+        
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state) { }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            // Important: ComponentLookups must be updated every frame.
+            // This syncs them with the latest state of the world.
+            _TransformLookup.Update(ref state);
+            _VelocityLookup.Update(ref state);
+            _entityTypeHandle.Update(ref state);
+            _SourceBufferTypeHandle.Update(ref state);
+            _ValueBufferTypeHandle.Update(ref state);
+
+            var job = new GatherObservationsJob
+            {
+                EntityTypeHandle = _entityTypeHandle,
+                // Get the handles for the buffer types. These tell the job how to access the data in a chunk.
+                SourceBufferTypeHandle = _SourceBufferTypeHandle, 
+                ValueBufferTypeHandle = _ValueBufferTypeHandle, 
+            
+                // Pass the updated lookups to the job.
+                TransformLookup = _TransformLookup,
+                VelocityLookup = _VelocityLookup,
+            };
+
+            // Schedule the job to run in parallel on all chunks matching our query.
+            state.Dependency = job.ScheduleParallel(_AgentQuery, state.Dependency);
+        }
+        
+        [BurstCompile]
+        private struct GatherObservationsJob : IJobChunk
+        {
+            // --- CHANGED: Added handle to get entities ---
+            public EntityTypeHandle EntityTypeHandle; 
+            
+            [ReadOnly] public BufferTypeHandle<ObservationSource> SourceBufferTypeHandle;
+            public BufferTypeHandle<ObservationValue> ValueBufferTypeHandle;
+
+            [ReadOnly] public ComponentLookup<LocalTransform> TransformLookup;
+            [ReadOnly] public ComponentLookup<PhysicsVelocity> VelocityLookup;
+
+            public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
+            {
+                // --- CHANGED: Get a NativeArray of all entities in the chunk first ---
+                // CORRECTED LINE: The 'ref' keyword is removed. The handle is passed by value.
+                NativeArray<Entity> entities = chunk.GetNativeArray(EntityTypeHandle);
+
+                BufferAccessor<ObservationSource> sourceAccessor = chunk.GetBufferAccessor(ref SourceBufferTypeHandle);
+                BufferAccessor<ObservationValue> valueAccessor = chunk.GetBufferAccessor(ref ValueBufferTypeHandle);
+
+                for (int i = 0; i < chunk.Count; i++)
+                {
+                    // --- CHANGED: Get the current entity from the array ---
+                    var entity = entities[i];
+                    
+                    DynamicBuffer<ObservationSource> sources = sourceAccessor[i];
+                    DynamicBuffer<ObservationValue> values = valueAccessor[i];
+
+                    for (int j = 0; j < sources.Length; j++)
+                    {
+                        ObservationSource source = sources[j];
+                        float observation = 0f;
+
+                        switch (source.SourceType)
+                        {
+                            case ObservationSourceType.PositionX:
+                                if (TransformLookup.HasComponent(entity))
+                                    observation = TransformLookup[entity].Position.x;
+                                break;
+                            case ObservationSourceType.PositionY:
+                                if (TransformLookup.HasComponent(entity))
+                                    observation = TransformLookup[entity].Position.y;
+                                break;
+                            case ObservationSourceType.PositionZ:
+                                if (TransformLookup.HasComponent(entity))
+                                    observation = TransformLookup[entity].Position.z;
+                                break;
+
+                            case ObservationSourceType.VelocityX:
+                                if (VelocityLookup.HasComponent(entity))
+                                    observation = VelocityLookup[entity].Linear.x;
+                                break;
+                            case ObservationSourceType.VelocityY:
+                                if (VelocityLookup.HasComponent(entity))
+                                    observation = VelocityLookup[entity].Linear.y;
+                                break;
+                            case ObservationSourceType.VelocityZ:
+                                if (VelocityLookup.HasComponent(entity))
+                                    observation = VelocityLookup[entity].Linear.z;
+                                break;
+                        }
+                        
+                        values[j] = observation;
+                    }
+                }
+            }
+        }
+    }
+}
