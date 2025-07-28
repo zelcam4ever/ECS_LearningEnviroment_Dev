a8e59a214e5cb1f18012c9c73a6078b7aa0c8a49
zelcam4ever <ezequiel.padillo.01@gmail.com>
2025-07-28 20:17:11 +0200

    refactor: policies component tags

--- Assets/Scripts/Data/Brain.cs
+++ Assets/Scripts/Data/Brain.cs
@@ -6,20 +6,13 @@ using Unity.MLAgents.Actuators;
 
 namespace EcsTraining
 {
-    public struct BrainParameter: IComponentData
-    {
-        public int VectorObservationSize;
-        public int StackedVectorObservation;
-        //public ActionSpec ActionType;
-        public int ActionSize;
-        public int TeamId;
-    }
-    
     public struct BrainSimple: IComponentData
     {
         public FixedString32Bytes FullyQualifiedBehaviorName;
     }
-
     
-   
+    public struct RemotePolicy : IComponentData { }
+    public struct InferencePolicy : IComponentData { }
+    public struct HeuristicPolicy : IComponentData { }
+    
 }
