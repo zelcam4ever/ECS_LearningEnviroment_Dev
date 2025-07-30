529bb3e20c3903a84a24f65886c93624ff5c1e19
zelcam4ever <ezequiel.padillo.01@gmail.com>
2025-07-30 22:40:27 +0200

    feat: sample usage implemented

---
+++ Assets/Sample/Scripts/AgentAuthoring.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using System.Collections.Generic;
+using Zelcam4.MLAgents;
+
+// Register all the generic component types this authoring script can create.
+
+
+public class AgentAuthoring : MonoBehaviour
+{
+    // Expose lists for each observation type in the Inspector.
+    public List<TransformConfig> TransformObservations;
+    
+    public class AgentObserverBaker : Baker<AgentAuthoring>
+    {
+        public override void Bake(AgentAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            int currentIndex = 0; 
+
+            // Repeat for each configuration of observations 
+            if (authoring.TransformObservations.Count > 0)
+            {
+                var requestBuffer = AddBuffer<ObservationRequest<LocalTransform>>(entity);
+                foreach (var config in authoring.TransformObservations)
+                {
+                    requestBuffer.Add(new ObservationRequest<LocalTransform>
+                    {
+                        SourceSubId = (byte)config.SourceType,
+                        TargetIndex = currentIndex
+                    });
+                    currentIndex++; // Increment the index for the next observation
+                }
+            }
+            
+            // Values to send buffer
+            var valueBuffer = AddBuffer<ObservationValue>(entity);
+            if (currentIndex > 0)
+            {
+                valueBuffer.ResizeUninitialized(currentIndex);
+            }
+        }
+    }
+}
---
+++ Assets/Sample/Scripts/ExampleTransformObservation.cs
@@ -0,0 +1,33 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Transforms;
+using Zelcam4.MLAgents;
+using Unity.Entities;
+
+[BurstCompile]
+public partial class ExampleVelocityObservationSystem : SystemBase
+{
+    private EntityQuery _queryTransform;
+    
+    protected override void OnCreate()
+    {
+        _queryTransform = new EntityQueryBuilder(Allocator.Temp)
+            .WithAll<ObservationValue, ObservationRequest<LocalTransform>, LocalTransform>()
+            .Build(this);
+    }
+
+
+    protected override void OnUpdate()
+    {
+        var jobTransform = new GatherJob<LocalTransform, TransformExtractor>
+        {
+            FinalObservationBufferHandle = GetBufferTypeHandle<ObservationValue>(false),
+            RequestsHandle = GetBufferTypeHandle<ObservationRequest<LocalTransform>>(true),
+            SourceComponentHandle = GetComponentTypeHandle<LocalTransform>(true),
+            Extractor = default
+        };
+        
+        Dependency = jobTransform.ScheduleParallel(_queryTransform, Dependency);
+
+    }
+}
