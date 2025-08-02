35a0e4d48677960b1cc813a6c402456cffd525bd
zelcam4ever <ezequiel.padillo.01@gmail.com>
2025-08-02 17:40:12 +0200

    feat: End episode user sample

---
+++ Assets/Sample/Scripts/EpisodeCompletedSystem.cs
@@ -0,0 +1,40 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Zelcam4.MLAgents;
+
+namespace Sample.Scripts
+{
+    [UpdateInGroup(typeof(EpisodeCompletedGroup))]
+    public partial struct EpisodeCompletedSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.World.Unmanaged);
+            
+            var job = new TerminateOnMaxStepsJob
+            {
+                ECB = ecb.AsParallelWriter()
+            };
+            state.Dependency = job.ScheduleParallel(state.Dependency);
+        }
+    }
+    
+    [BurstCompile]
+    public partial struct GoalReachedJob : IJobEntity
+    {
+        public EntityCommandBuffer.ParallelWriter ECB;
+
+        private void Execute([ChunkIndexInQuery] int entityInQueryIndex, Entity entity, ref AgentEcs agent, in DynamicBuffer<ObservationValue> observations)
+        {
+            if (math.distance(new float2(observations[0].Value, observations[1].Value), 
+                    new float2(observations[2].Value, observations[3].Value)) < 0.5f)
+            {
+                agent.MaxStepReached = false;
+                ECB.AddComponent<EndEpisodeTag>(entityInQueryIndex, entity);
+            }
+        }
+    }
+}
