d8ff620795f7c2a45b20f5c7c7a39192a4848e46
zelcam4ever <ezequiel.padillo.01@gmail.com>
2025-08-03 19:21:00 +0200

    chore: removing files track in this repo

--- Packages/com.zelcam4.ml-agents/Runtime/Grpc/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Zelcam4.ML-Agents")]
-[assembly: InternalsVisibleTo("Zelcam4.ML-Agents.Editor")]
-[assembly: InternalsVisibleTo("Zelcam4.ML-Agents.Editor.Tests")]
-[assembly: InternalsVisibleTo("Zelcam4.ML-Agents.Runtime.Sensor.Tests")]
-[assembly: InternalsVisibleTo("Zelcam4.ML-Agents.Runtime.Utils.Tests")]
--- Packages/com.zelcam4.ml-agents/Runtime/Systems/ActionSystemGroup.cs
+++
@@ -1,7 +0,0 @@
-using Unity.Entities;
-
-namespace Zelcam4.MLAgents
-{
-    [UpdateAfter(typeof(DecideActionSystem))]
-    public partial class ActionSystemGroup : ComponentSystemGroup {}
-}
